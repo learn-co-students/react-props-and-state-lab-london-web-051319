11e67f336ee61c0feb489f487ac2298f23d35d47
moniet <moniet.sawhney@gmail.com>
2019-07-16 16:39:24 +0100

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,59 @@
-import React from 'react'
+import React from 'react';
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: 'all',
+      },
+    };
   }
+
+  updateFilter = e => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: e.target.value,
+      },
+    });
+  };
+
+  fetchPets = () => {
+    let filter = this.state.filters.type;
+    if (filter === 'all') {
+      this.filterAll();
+    } else {
+      this.filterSpecies(filter);
+    }
+  };
+
+  filterAll = (animalName) => {
+    fetch('/api/pets')
+     .then(res => res.json())
+     .then(pets => this.setState(pets));
+  };
+
+  filterSpecies = (animal) => {
+    fetch(`/api/pets?type=${animal}`)
+     .then(res => res.json())
+     .then(pets => {
+        this.setState({ pets });
+      }
+     );
+  };
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets });
+  };
 
   render() {
     return (
@@ -24,16 +64,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.updateFilter}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
