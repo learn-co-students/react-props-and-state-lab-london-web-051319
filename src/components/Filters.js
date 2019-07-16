b42769fa186d6548fdeb698c75c6783a91e0c153
Alex Johnston <alexjohnston@ymail.com>
2019-07-16 16:58:52 +0100

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,25 +1,19 @@
 import React from 'react'
 
-class Filters extends React.Component {
-  render() {
-    return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
-
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
+function Filters (props) {
+  return (
+    <div className='ui form'>
+      <h3>Animal type</h3>
+      <div className='field'>
+        <select onChange={e => props.updateFilters(e.target.value)} name='type' id='type'>
+          <option value='all'>All</option>
+          <option value='cat'>Cats</option>
+          <option value='dog'>Dogs</option>
+          <option value='micropig'>Micropigs</option>
+        </select>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Filters
