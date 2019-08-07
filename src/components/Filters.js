d55c72dda8a6406ab358a7406193cc4bb44e99de
Avelyne0 <sam.dbarrie@gmail.com>
2019-08-07 13:03:20 +0100

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,29 +1,25 @@
 import React from 'react'
 
-class Filters extends React.Component {
-  render() {
-    return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type" onChange={this.props.onChangeType}>
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
-
-        <div className="field">
-          <button
-            onClick={this.props.onFindPetsClick}
-            className="ui secondary button">
-            Find pets
-          </button>
-        </div>
+export default function Filters({onFindPetsClick, onChangeType}) {
+  return (
+    <div className="ui form">
+      <h3>Animal type</h3>
+      <div className="field">
+        <select name="type" id="type" onChange={onChangeType}>
+          <option value="all">All</option>
+          <option value="cat">Cats</option>
+          <option value="dog">Dogs</option>
+          <option value="micropig">Micropigs</option>
+        </select>
       </div>
-    )
-  }
-}
 
-export default Filters
+      <div className="field">
+        <button
+          onClick={onFindPetsClick}
+          className="ui secondary button">
+          Find pets
+          </button>
+      </div>
+    </div>
+  )
+}
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,19 +1,14 @@
 import React from 'react'
-
 import Pet from './Pet'
 
-class PetBrowser extends React.Component {
-  render() {
-    return (
-      <div className="ui cards">
-        {
-          this.props.pets.map(pet => (
-            <Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet} />
-          ))
-        }
-      </div>
-    )
-  }
+export default function PetBrowser({ pets, onAdoptPet }) {
+  return (
+    <div className="ui cards">
+      {
+        pets.map(pet => (
+          <Pet pet={pet} key={pet.id} onAdoptPet={onAdoptPet} />
+        ))
+      }
+    </div>
+  )
 }
-
-export default PetBrowser
